d151832afb8bf37bf3a5fc81b4b7dcc85ec9d260
Антон Николаев <whiteheron819@yandex.ru>
2022-02-26 10:17:09 +0300

    All tasks solved

--- tasks/lambda_function/task_1/implementation.py
+++ tasks/lambda_function/task_1/implementation.py
@@ -1,2 +1,2 @@
 my_list = [(2, 2), (6, 13), (55, 3), (4, 8)]
-res_list = []
+res_list = sorted(my_list, reverse=True, key=lambda point: point[1])
--- tasks/magic_method/task_2/implementation.py
+++ tasks/magic_method/task_2/implementation.py
@@ -1,8 +1,37 @@
+import datetime as dt
+
+
 class MathClock:
+
     def __init__(self):
-        pass
+        """
+        Задаём начальное время
+        """
+        self.time = dt.datetime.strptime('00:00', '%H:%M')
+
+    def __add__(self, other):
+        """
+        Функция увеличения количества минут, работающая при сложении с int
+        """
+        self.time += dt.timedelta(minutes=other)
+
+    def __sub__(self, other):
+        """
+        Функция уменьшения количества минут, работающая при вычитании с int
+        """
+        self.time -= dt.timedelta(minutes=other)
+
+    def __mul__(self, other):
+        """
+        Функция увеличения количества часов, работающая при умножении на int
+        """
+        self.time += dt.timedelta(hours=other)
+
+    def __truediv__(self, other):
+        """
+        Функция уменьшения количества часов, работающая при делении на int
+        """
+        self.time -= dt.timedelta(hours=other)
 
     def get_time(self):
-        return 0
-
-
+        return self.time.strftime('%H:%M')
